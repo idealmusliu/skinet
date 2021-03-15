c5b1d5b84bc698ebd79c2cd887b221509f7cd412
Ideal Musliu <ideal.musliu@abc-techgroup.com>
2021-03-15 13:18:54 +0100

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _conext;
+
+        public ProductsController(StoreContext conext)
+        {
+            _conext = conext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _conext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _conext.Products.FindAsync(id);
+        }
+    }
+}
