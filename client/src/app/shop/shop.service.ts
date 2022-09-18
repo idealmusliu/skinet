e21fa21d31c6d2c0b7bc50c26af5000815b9eb34
Ideal Musliu <ideal_h95@hotmail.com>
2022-09-18 05:29:05 +0200

    Created Basket on the Client Side

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, map } from 'rxjs';
+import { map } from 'rxjs';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IProduct } from '../shared/models/product';
